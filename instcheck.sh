467ab80d8bab09fbf9c89065668286f020a6bbaf
Interceptv <97058207+Interceptv@users.noreply.github.com>
2023-08-31 01:14:41 -0300

    Update instcheck.sh

--- instcheck.sh
+++ instcheck.sh
@@ -6,7 +6,7 @@ dpkg-reconfigure --frontend noninteractive tzdata > /dev/null 2>&1
 clear
 echo -e "\E[44;1;37m    INSTALAR CHECKUSER CONECTA4G     \E[0m" 
 echo ""
-echo -e "                 \033[1;31mBy @scvirtual\033[1;36m"
+echo -e "                 \033[1;31mBy @Sr_Wallker\033[1;36m"
 echo ""
 echo -ne "\n\033[1;32mDE UM ENTER PARA \033[1;33mCONTINUAR...\033[1;37m: "; read -r
 clear
@@ -17,13 +17,13 @@ rm /bin/chuser > /dev/null 2>&1
 sleep 5
 cd /bin || exit
 wget https://github.com/Interceptv/scanny/raw/main/chuser > /dev/null 2>&1
-wget https://github.com/wellzin-blip/new2022/raw/master/Painel-V2022/userscheck > /dev/null 2>&1
+wget https://github.com/Interceptv/scanny/raw/main/userscheck > /dev/null 2>&1
 chmod 777 chuser > /dev/null 2>&1
 chmod 777 userscheck > /dev/null 2>&1
 clear
 mkdir /etc/rec > /dev/null 2>&1
-echo -e 'By: @scvirtual' > /etc/rec/licence
-echo -e 'By: @scvirtual' > /usr/lib/licence
+echo -e 'By: @KIRITO_SSH' > /etc/rec/licence
+echo -e 'By: @KIRITO_SSH' > /usr/lib/licence
 mkdir /usr/lib/checkuser > /dev/null 2>&1
 cd /usr/lib/checkuser || exit
 rm checkuser.py > /dev/null 2>&1
@@ -36,6 +36,6 @@ clear
 echo ""
 echo -e "\033[1;31m \033[1;33mCOMANDO PRINCIPAL: \033[1;32mchuser\033[0m"
 echo ""
-echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@scvirtual\033[0m"
+echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@Sr_Wallker\033[0m"
 cat /dev/null > ~/.bash_history && history -c
 exit
