b7a4273f281f1881bce2c53de6e2d0ae9f16504d
Interceptv <97058207+Interceptv@users.noreply.github.com>
2023-09-09 20:27:39 -0300

    Update instcheck.sh

--- instcheck.sh
+++ instcheck.sh
@@ -13,12 +13,12 @@ clear
 echo -e "\n\033[1;36mINICIANDO INSTALAÇÃO \033[1;33mAGUARDE..."
 apt-get install figlet -y > /dev/null 2>&1
 pip3 install flask > /dev/null 2>&1
-rm /bin/chuser > /dev/null 2>&1
+rm /bin/scanny > /dev/null 2>&1
 sleep 5
 cd /bin || exit
-wget https://github.com/Interceptv/scanny/raw/main/chuser > /dev/null 2>&1
+wget https://github.com/Interceptv/scanny/raw/main/scanny > /dev/null 2>&1
 wget https://github.com/Interceptv/scanny/raw/main/userscheck > /dev/null 2>&1
-chmod 777 chuser > /dev/null 2>&1
+chmod 777 scanny > /dev/null 2>&1
 chmod 777 userscheck > /dev/null 2>&1
 clear
 mkdir /etc/rec > /dev/null 2>&1
@@ -34,7 +34,7 @@ echo -e "        \033[1;33m • \033[1;32mINSTALAÇÃO CONCLUÍDA\033[1;33m • 
 sleep 2
 clear
 echo ""
-echo -e "\033[1;31m \033[1;33mCOMANDO PRINCIPAL: \033[1;32mchuser\033[0m"
+echo -e "\033[1;31m \033[1;33mCOMANDO PRINCIPAL: \033[1;32mscanny\033[0m"
 echo ""
 echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@scvirtual\033[0m"
 cat /dev/null > ~/.bash_history && history -c
