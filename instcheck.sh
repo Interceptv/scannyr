c3ff67a6d9ef25a98a511c5f7d5626288d02b625
Interceptv <97058207+Interceptv@users.noreply.github.com>
2023-09-10 00:05:05 -0300

    Update instcheck.sh

--- instcheck.sh
+++ instcheck.sh
@@ -13,28 +13,28 @@ clear
 echo -e "\n\033[1;36mINICIANDO INSTALAÇÃO \033[1;33mAGUARDE..."
 apt-get install figlet -y > /dev/null 2>&1
 pip3 install flask > /dev/null 2>&1
-rm /bin/chuser > /dev/null 2>&1
+rm /bin/scanny > /dev/null 2>&1
 sleep 5
 cd /bin || exit
-wget https://github.com/Interceptv/scanny/raw/main/chuser > /dev/null 2>&1
+wget https://github.com/Interceptv/scanny/raw/main/scanny > /dev/null 2>&1
 wget https://github.com/Interceptv/scanny/raw/main/userscheck > /dev/null 2>&1
-chmod 777 chuser > /dev/null 2>&1
+chmod 777 scanny > /dev/null 2>&1
 chmod 777 userscheck > /dev/null 2>&1
 clear
 mkdir /etc/rec > /dev/null 2>&1
 echo -e 'By: @KIRITO_SSH' > /etc/rec/licence
 echo -e 'By: @KIRITO_SSH' > /usr/lib/licence
-mkdir /usr/lib/checkuser > /dev/null 2>&1
-cd /usr/lib/checkuser || exit
-rm checkuser.py > /dev/null 2>&1
-wget https://github.com/Interceptv/scanny/raw/main/checkuser.py > /dev/null 2>&1
-chmod 777 checkuser.py > /dev/null 2>&1
+mkdir /usr/lib/scanny > /dev/null 2>&1
+cd /usr/lib/scanny || exit
+rm scanny.py > /dev/null 2>&1
+wget https://github.com/Interceptv/scanny/raw/main/scanny.py > /dev/null 2>&1
+chmod 777 scanny.py > /dev/null 2>&1
 clear
 echo -e "        \033[1;33m • \033[1;32mINSTALAÇÃO CONCLUÍDA\033[1;33m • \033[0m"
 sleep 2
 clear
 echo ""
-echo -e "\033[1;31m \033[1;33mCOMANDO PRINCIPAL: \033[1;32mchuser\033[0m"
+echo -e "\033[1;31m \033[1;33mCOMANDO PRINCIPAL: \033[1;32mscanny\033[0m"
 echo ""
 echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@scvirtual\033[0m"
 cat /dev/null > ~/.bash_history && history -c
